21ec10e88c4ff5464a28fb6b2a540a69a78ce9d3
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-06-30 19:58:00 -0400

    custom select scss

--- resources/views/blog/admin/partials/form.blade.php
+++ resources/views/blog/admin/partials/form.blade.php
@@ -4,3 +4,17 @@
 </div>
 
 <textarea id="mirrormark1" name="markdown"></textarea>
+
+<div class="custom-select">
+<select name="category_id">
+	<option selected disabled>Select a Category</option>
+	@foreach ($categories as $category)
+	<option value="{{ $category->id }}">{{ $category->name }}</option>
+	@endforeach
+</select>
+</div>
+
+<div>
+	<button type="submit" name="published" value="0">Save Draft</button>
+	<button type="submit" name="published" value="1">Publish</button>
+</div>
